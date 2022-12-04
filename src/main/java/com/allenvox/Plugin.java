12f0fedc8eb578432c3a6a1dd1c442ed124f45bf
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-12-04 12:25:43 +0700

    Connect event listener to main plugin class

--- src/main/java/com/allenvox/Plugin.java
+++ src/main/java/com/allenvox/Plugin.java
@@ -1,17 +1,19 @@
 package com.allenvox;
 
 import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Plugin extends JavaPlugin {
   public static Logger logger = Logger.getLogger("fine");
-  private CommandHandler commandHandler;
 
   public void onEnable() {
     logger.info("Fine plugin enabled");
-    commandHandler = new CommandHandler();
-    getCommand("hi").setExecutor(commandHandler);
+    getServer().getPluginManager().registerEvents(new EventListener(), this);
+    getCommand("hi").setExecutor(new CommandHandler());
   }
 
-  public void onDisable() { logger.info("Fine plugin disabled"); }
+  public void onDisable() {
+    logger.info("Fine plugin disabled");
+  }
 }
