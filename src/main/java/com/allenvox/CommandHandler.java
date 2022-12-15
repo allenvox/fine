7ea0725af0de71dd9d255b0ba4fd4df3a3e1845f
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-12-15 17:24:28 +0700

    Implement basic '/coords' command handling

--- src/main/java/com/allenvox/CommandHandler.java
+++ src/main/java/com/allenvox/CommandHandler.java
@@ -1,5 +1,6 @@
 package com.allenvox;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -8,12 +9,27 @@ import org.bukkit.entity.Player;
 public class CommandHandler implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(command.getName().equalsIgnoreCase("hi") && sender instanceof Player) {
+        if(command.getName().equalsIgnoreCase("coords") && sender instanceof Player) {
             Player player = (Player) sender;
-            player.sendMessage("hello! :)");
-            return true;
+            if(args.length != 3) {
+                player.sendMessage(ChatColor.GREEN + "Saved coordinates:");
+                return true;
+            } else {
+                int x, z;
+                try {
+                    x = Integer.parseInt(args[0]);
+                    z = Integer.parseInt(args[1]);
+                }
+                catch(NumberFormatException e) {
+                    player.sendMessage(ChatColor.RED + "X and Z values must be numeric!");
+                    return false;
+                }
+                String name = args[2];
+                player.sendMessage(ChatColor.GREEN + "Succesfully saved " + ChatColor.YELLOW + "(" + x + "; " + z + ")" + ChatColor.GREEN + " as " + ChatColor.GOLD + name);
+                return true;
+            }
         } else {
-            Plugin.logger.info("Command not found");
+            Plugin.logger.info("Command /"+ command.getName() + " not found");
             return false;
         }
     }
