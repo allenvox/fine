9efe168f89a73320c8a5f98973a0b6056959e184
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-12-16 22:55:09 +0700

    Add handling JSON with reading/writing coordinates

--- src/main/java/com/allenvox/CommandHandler.java
+++ src/main/java/com/allenvox/CommandHandler.java
@@ -1,12 +1,18 @@
 package com.allenvox;
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class CommandHandler implements CommandExecutor {
     private Plugin plugin;
@@ -21,6 +27,21 @@ public class CommandHandler implements CommandExecutor {
             Player player = (Player) sender;
             if(args.length != 3) {
                 player.sendMessage(ChatColor.GREEN + "Saved coordinates:");
+                File file = new File(plugin.getDataFolder(), "coords.json");
+                try {
+                    JSONParser jsonParser = new JSONParser();
+                    Object parsed = jsonParser.parse(new FileReader(file.getPath()));
+                    JSONObject jsonObject = (JSONObject) parsed;
+                    JSONArray pl = (JSONArray) jsonObject.get(player.getName());
+                    for(Object obj : pl) {
+                        JSONObject place = (JSONObject) obj;
+                        player.sendMessage(ChatColor.GOLD + place.get("name").toString() + ChatColor.GREEN + ":");
+                        player.sendMessage(ChatColor.GREEN + "X: " + place.get("x").toString() + ChatColor.GREEN + ", Z: " + place.get("z").toString());
+                    }
+                }
+                catch(ParseException | IOException e) {
+                    e.printStackTrace();
+                }
                 return true;
             } else {
                 int x, z;
@@ -33,8 +54,22 @@ public class CommandHandler implements CommandExecutor {
                     return false;
                 }
                 String name = args[2];
-                File path = plugin.getDataFolder();
+                File file = new File(plugin.getDataFolder(), "coords.json");
+                try {
+                    JSONObject place = new JSONObject();
+                    place.put("name", name);
+                    place.put("x", x);
+                    place.put("z", z);
 
+                    JSONParser jsonParser = new JSONParser();
+                    Object parsed = jsonParser.parse(new FileReader(file.getPath()));
+                    JSONObject jsonObject = (JSONObject) parsed;
+                    JSONArray pl = (JSONArray) jsonObject.get(player.getName());
+                    pl.add(place);
+                }
+                catch(ParseException | IOException e) {
+                    e.printStackTrace();
+                }
                 player.sendMessage(ChatColor.GREEN + "Succesfully saved " + ChatColor.YELLOW + "(" + x + "; " + z + ")" + ChatColor.GREEN + " as " + ChatColor.GOLD + name);
                 return true;
             }
