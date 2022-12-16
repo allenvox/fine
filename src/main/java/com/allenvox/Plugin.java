e5f1742a550d9991358cf33c1eb971d9d62328c1
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2022-12-16 21:25:50 +0700

    Change CommandHandler init with new constructor

--- src/main/java/com/allenvox/Plugin.java
+++ src/main/java/com/allenvox/Plugin.java
@@ -6,11 +6,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Plugin extends JavaPlugin {
   public static Logger logger = Logger.getLogger("fine");
-
   public void onEnable() {
     logger.info("Fine plugin enabled");
     getServer().getPluginManager().registerEvents(new EventListener(), this);
-    getCommand("coords").setExecutor(new CommandHandler());
+    getCommand("coords").setExecutor(new CommandHandler(this));
   }
 
   public void onDisable() {
