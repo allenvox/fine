e099c9d39594fbb6303dea49f084b709150849a1
Yuriy Grigoryev <allenvox@users.noreply.github.com>
2023-01-23 16:07:04 +0700

    Format code

--- src/main/java/com/allenvox/Plugin.java
+++ src/main/java/com/allenvox/Plugin.java
@@ -5,14 +5,15 @@ import java.util.logging.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Plugin extends JavaPlugin {
-  public static Logger logger = Logger.getLogger("fine");
-  public void onEnable() {
-    logger.info("Fine plugin enabled");
-    getServer().getPluginManager().registerEvents(new EventListener(), this);
-    getCommand("coords").setExecutor(new CommandHandler(this));
-  }
+    public static Logger logger = Logger.getLogger("fine");
+    
+    public void onEnable() {
+        logger.info("Fine plugin enabled");
+        getServer().getPluginManager().registerEvents(new EventListener(), this);
+        getCommand("coords").setExecutor(new CommandHandler(this));
+    }
 
-  public void onDisable() {
-    logger.info("Fine plugin disabled");
-  }
+    public void onDisable() {
+        logger.info("Fine plugin disabled");
+    }
 }
